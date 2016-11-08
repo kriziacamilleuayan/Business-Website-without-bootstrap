aec96fd3035e52c525e0a077761743974267d407
kcNeko <kriziacamilleuayan@gmail.com>
2016-11-08 15:52:48 +0800

    2nd commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+document.getElementById("lorem").innerHTML = lorem_ipsum;
+document.getElementById("lorem2").innerHTML = lorem_ipsum;
+document.getElementById("lorem3").innerHTML = lorem_ipsum;
+document.getElementById("lorem4").innerHTML = lorem_ipsum;
+document.getElementById("lorem5").innerHTML = lorem_ipsum;
